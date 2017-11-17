3fee107e2e5fb22ad0ced151716a15653baa2843
AJ Jones <ajjonez@icloud.com>
2017-11-17 12:46:34 -0600

    clean js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-var maxHeight = -1;
-$('.slick-slide').each(function() {
-  if ($(this).height() > maxHeight) {
-    maxHeight = $(this).height();
-  }
-});
-$('.slick-slide').each(function() {
-  if ($(this).height() < maxHeight) {
-    $(this).css('margin', Math.ceil((maxHeight-$(this).height())/2) + 'px 0');
-  }
-});
+
 
 jQuery(document).ready(function($){
 	$('.shareable-img').on('click', function(){
