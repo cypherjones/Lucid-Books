75771ba07cc0926baa84fd3da8e19d5512ad85a8
cypherjones <ajjonez@icloud.com>
2017-11-17 21:29:15 -0600

    fixing js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 jQuery(document).ready(function($){
-	$('.shareable-img')
-	.on('click', function(){
-		$(this)
-		.find('.shareable-popup', this)
-		.addClass('open');
-	});
+	// $('.shareable-img')
+	// .on('click', function(){
+	// 	$(this)
+	// 	.find('.shareable-popup', this)
+	// 	.addClass('open');
+	// });
 
-	$('.close-popup')
-	.on('click', function(){
+	// $('.close-popup')
+	// .on('click', function(){
 
-		$(this)
-		.parent('.shareable-img')
-		.find('.shareable-popup', this)
-		.removeClass('open');
+	// 	$(this)
+	// 	.parent('.shareable-img')
+	// 	.find('.shareable-popup', this)
+	// 	.removeClass('open');
 
+	// })
+$('.shareable-img').each(function(){
+
+	$(this).find('.shareable-popup').on('click', function(){
+		console.log('yes');
 	})
-});
+
+})
+
+ 
