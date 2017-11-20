c8e5bb1ff1849238619e714139f39fe1643662c0
cypherjones <ajjonez@icloud.com>
2017-11-19 22:27:24 -0600

    try next

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function($){
 
 			console.log("testing bitch")
 
-			if($(this).next('.shareable-popup:visible').length == 0) {
+			if($('.shareable-popup:visible').length == 0) {
 
 				$('.shareable-popup').fadeIn("slow");
 
