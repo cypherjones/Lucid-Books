e8404c4f903aa986179207a0c0e838ac6a589be6
AJ Jones <ajjonez@icloud.com>
2017-12-04 14:59:50 -0600

    debug js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 jQuery(document).ready(function($){
 	
- 
+ $('imagebox')each(function(){
 
+ 	$this = $(this);
 
+ 	let i = 0;
+
+ 	// for (var i = this.length - 1; i >= 0; i--) {
+ 	// 	Things[i]
+ 	// }
+
+ 	console.log($this);
+
+ });
 });
