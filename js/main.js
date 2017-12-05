31304310b02c147a874eb71615466c341b284952
AJ Jones <ajjonez@icloud.com>
2017-12-05 00:06:51 -0600

    debug js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($){
 	$('.share-container').each(function(){
 
 		$(this).find('.shareable-img', this).each(function(index, value){
-			console.log(`{index} : {value}`);
+			console.log(`${index} : ${value}`);
 		})
 
 	});
