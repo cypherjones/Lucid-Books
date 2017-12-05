e769c9c9a7601b59db3cff9f1bd8b7a633358a65
AJ Jones <ajjonez@icloud.com>
2017-12-05 00:01:52 -0600

    debug js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 
 	$('.share-container').each(function(){
 
-		$(this).find('#popup-container', this).each(function(){
+		$(this).find('.shareable-img', this).each(function(){
 			console.log('yes, sir');
 		})
 
