9583f1ebe32de34fcdca47511b63c27b674e2d9d
AJ Jones <ajjonez@icloud.com>
2017-12-05 17:54:50 -0600

    debug js --> addclasses in active

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ jQuery(document).ready(function($){
 		$('.next-pop').on('click', function(){
 			let $n = $(this).closest('.shareable-popup').attr('id');
 			
-			let $ne = $(this).offsetParent('.col-md-3').html();
+			let $ne = $(this).parent('.col-md-3').html();
 
 			// let $m = _n.attr('id');
 
