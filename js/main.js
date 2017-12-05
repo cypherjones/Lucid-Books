fdd55b8595372fd9f955c7d67312e79cc50303fd
AJ Jones <ajjonez@icloud.com>
2017-12-05 17:24:29 -0600

    debug js --> addclasses in active

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function($){
 			$(this).each(function(){
 
 			let n = $(this);
-			let $next = n.next().text('foo');
+			let $next = n.nextAll().text('foo');
 
 			let l = n.attr('id');
 			let m = $next.attr('id');
