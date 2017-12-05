b47b6a9f6ef525d6c5e0f556f581eef9ed6d2f72
AJ Jones <ajjonez@icloud.com>
2017-12-04 22:33:49 -0600

    debug js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,7 @@ jQuery(document).ready(function($){
 	
  $('#popup-container').each(function(){
 
- 	$this = $(this).text();
-
- 	
+ 	$this = $(this);
 
  	for (let i = 0; l = $this.length; i < l; i++) {
  		
