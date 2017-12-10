3b30587b05b5b0dadceb90709cf6b93eff7ed9f1
AJ Jones <ajjonez@icloud.com>
2017-12-09 22:28:26 -0600

    try fb sdk again

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
 jQuery(document).ready(function($){
 
+	 window.fbAsyncInit = function() {
+	FB.init({
+	  appId            : '284281862094433',
+	  autoLogAppEvents : true,
+	  xfbml            : true,
+	  version          : 'v2.10'
+	});
+	FB.AppEvents.logPageView();
+  };
+ 
+  (function(d, s, id){
+	 var js, fjs = d.getElementsByTagName(s)[0];
+	 if (d.getElementById(id)) {return;}
+	 js = d.createElement(s); js.id = id;
+	 js.src = "//connect.facebook.net/en_US/sdk.js";
+	 fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+  function shareOverrideOGMeta(overrideLink, overrideTitle, overrideDescription, overrideImage) {
+		FB.ui({
+			method: 'share_open_graph',
+			action_type: 'og.shares',
+			action_properties: JSON.stringify({
+				object: {
+					'og:url': overrideLink,
+					'og:title': overrideTitle,
+					'og:description': overrideDescription,
+					'og:image': overrideImage
+				}
+			})
+		},
+		function (response) {
+		// Action after response
+		});
+	}
+
 	let $n = ''.
 			c = '',
 			n = '',
