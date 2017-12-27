37d2d390a9f08fb5610319eaca875fe55f96c81b
AJ Jones <ajjonez@icloud.com>
2017-12-26 23:26:56 -0600

    update

--- share.php
+++ share.php
@@ -25,11 +25,12 @@
 				  if ( $share->have_posts() ) : while ( $share->have_posts() ) : $share->the_post();
 
 				  $page_id = get_the_id();
+				  $featured_img = get_the_post_thumbnail_url( $page_id, 'full' );
 
 				  ?>
 
 				  <div class="col-md-4">
-				  	<?php echo the_title( ); ?>
+				  	<img src="<?php echo $featured_img; ?>" alt="">
 				  </div>
 
 			<?php endwhile; endif; ?>
