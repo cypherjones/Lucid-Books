0d0d0e345bf9cd23a2f90824a06a48381b39e9fb
cypherjones <ajjonez@icloud.com>
2018-02-07 20:28:30 -0600

    now test share code - fix typo

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function($){
       e.preventDefault();
       // grab the image
       let $code = $(this).html(),
-          $share = $(this).closest('.share-me').hmtl();
+          $share = $(this).closest('.share-me').html();
       console.log($code, $share);
     })
   });
