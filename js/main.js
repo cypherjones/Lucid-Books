827617849fb5da6aa25bbe67659a2d9571aaeadc
cypherjones <ajjonez@icloud.com>
2018-02-07 20:22:41 -0600

    dont

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ jQuery(document).ready(function($){
 
   $('.popup').each(function(){
     $('.share-me').hide()
+
+    $(this).on('click', function (e) {
+      e.preventDefault();
+      console.log('we have lift off')
+    })
   });
  		
 }); 
