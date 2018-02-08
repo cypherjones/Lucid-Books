9c039e0308d9f54a31c4691676a15f9d70fa1951
cypherjones <ajjonez@icloud.com>
2018-02-07 20:18:17 -0600

    test hide share popup

--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
 jQuery(document).ready(function($){
 
 
-$(".popup").on("click", function(e) {
-      e.preventDefault();
-      windowPopup($(this).attr("href"), 500, 300);
-    });
-
- 		function windowPopup(url, width, height) {
-        // Calculate the position of the popup so
-        // it’s centered on the screen.
-        let left = (screen.width / 2) - (width / 2),
-            top = (screen.height / 2) - (height / 2);
-        window.open(
-          url,
-          "",
-          "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left
-        );
-      };
+  $('.popup').each(function(){
+    $('.share-me').hide()
+  });
  		
 }); 
