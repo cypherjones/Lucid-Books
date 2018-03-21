815dd6bfddcc6830c89f2bdec4bcd7665314f234
cypherjones <ajjonez@icloud.com>
2018-03-21 18:38:02 -0500

    add new js markup

--- js/main.js
+++ js/main.js
@@ -60,7 +60,6 @@ jQuery(document).ready(function($){
           );
         };
 
-      $('.share-btn').on('click', windowPopup);
 
       // close if we click outside of the popup
       $(document).mouseup(function(e){
