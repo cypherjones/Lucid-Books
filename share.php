17494c589a6f585c1748ee4d3f7c86f4401d19f0
cypherjones <ajjonez@icloud.com>
2018-04-03 12:51:34 -0500

    randomize share posts -- refactored

--- share.php
+++ share.php
@@ -5,27 +5,32 @@
 
   get_header(); 
 
+  $post_ids = array();
+
   	/*
   	 * The WordPress Query class.
   	 *
   	 * @link http://codex.wordpress.org/Function_Reference/WP_Query
   	 */
   	$args = array(
-  		'post_type'      => 'shareables',  
+  		'post_type'      => array('shareables'),  
+      'order_by' => 'rand',
   		'post_status'    => 'publish',
-  		'posts_per_page' => 1,
-      'order_by' => 'rand'
+  		'posts_per_page' => -1,
+      
 
   	);
   
   $share = new WP_Query( $args );
 
-  
+  while ( $share->have_posts() ) : $share->the_post();
+    $post_ids[] = $post->ID;
+  endwhile; wp_reset_query();
   
   ?>
 	<div class="container">
 		<div class="row">
-			<?php echo do_shortcode('[ajax_load_more id="4349616249" repeater="template_1" post_type="shareables" posts_per_page="9" image_loaded="true" button_label="more sharables" button_loading_label="loading sharables" orderby="rand"]') ?>
+			<?php echo do_shortcode('[ajax_load_more id="4349616249" repeater="template_1" post_in"'.implode(',', $post_ids) . '" orderby="post__in" posts_per_page="9" image_loaded="true" button_label="more sharables" button_loading_label="loading sharables"') ?>
 		</div>
 	</div>
  
